7fbe51fee6e7d30a4916989e1dc1d545c816ba6b
Serp <manu.serpette74@gmail.com>
2023-02-23 14:15:41 -0500

    1 2

---
+++ base1.pl
@@ -0,0 +1,13 @@
+animal(chien).
+animal(chat).
+prenom(paul).
+prenom(pierre).
+prenom(jean).
+possede(jean,chat).
+possede(pierre,chien).
+possede(pierre,cheval).
+amis(pierre,jean).
+amis(jean,pierre).
+amis(jean,paul).
+amis(pierre,paul).
+amis(paul,jacques).
---
+++ req.pl
@@ -0,0 +1,5 @@
+%1
+possede(X,chat)
+%2
+possede(pierre,X)
+%3
