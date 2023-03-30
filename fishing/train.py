1c26d1505d38001002800406cd410dc72833275a
MarcBettez <69468831+MarcBettez@users.noreply.github.com>
2023-03-30 16:23:59 -0400

    .

--- fishing/train.py
+++ fishing/train.py
@@ -40,12 +40,13 @@ def build_set(path):
     x = x.drop(columns=['Unnamed: 0'])
     x = x.to_numpy()
     x = normalize(x, axis=0)
-    y = df["status"].to_numpy()
-    for i, s in enumerate(y):
-        if s == "phishing":
-            y[i] = 1
-        else:
-            y[i] = 0
+    y = df["status"].map({'legitimate': 0, 'phishing': 1})
+    # y = df["status"].to_numpy()
+    # for i, s in enumerate(y):
+    #     if s == "phishing":
+    #         y[i] = [0, 1]
+    #     else:
+    #         y[i] = [1, 0]
     return x, y
 
 
@@ -55,24 +56,27 @@ x_train, x_test, y_train, y_test = train_test_split(
     x_train, y_train, test_size=0.3, shuffle=True)
 
 grid = {
-    'n_estimators': [500, 600, 800],
+    'n_estimators': [1000, 1200, 1400, 1600],
     'max_features': ['sqrt'],
-    'max_depth': [16, 20, 24, 28, 32],
+    'max_depth': [24, 28, 30, 32],
     'random_state': [18]
-}  # show start time
+}
+# show start time
 print(datetime.now())  # Grid Search function
-CV_rfr = GridSearchCV(estimator=RandomForestRegressor(),
+
+CV_rfr = GridSearchCV(estimator=RandomForestClassifier(),
                       param_grid=grid, cv=5, verbose=10, n_jobs=-1)
-CV_rfr.fit(x_train, y_train)  # show end time
+CV_rfr.fit(x_train, y_train)
+
+# show end time
 print(datetime.now())
 print("\n The best parameters across ALL searched params:\n", CV_rfr.best_params_)
 
 
-# rf = RandomForestClassifier(
-#     n_estimators=400, max_features='sqrt', max_depth=7, random_state=18)
+# rf = RandomForestClassifier(n_estimators=1200, max_features='sqrt', max_depth=28, random_state=18)
 
 # rf.fit(x_train, y_train)
-
+# print(rf.predict(x_test))
 prediction = filter(CV_rfr.predict(x_test))
 
 print("Accuracy: ", compute_accuracy(y_test, prediction))
