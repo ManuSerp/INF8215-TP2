cc471d065d7332fd5e2e8c4223332cdd7db5291d
MarcBettez <69468831+MarcBettez@users.noreply.github.com>
2023-04-13 15:17:27 -0400

    .

--- fishing/train.py
+++ fishing/train.py
@@ -1,16 +1,41 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from datetime import datetime
+import csv
 from sklearn.datasets import fetch_openml
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import normalize
+
 # Make NumPy printouts easier to read.
 np.set_printoptions(precision=3, suppress=True)
+
+
+def submission(L):
+    test = open("test.csv", "r")
+    with open("submission.csv", "w") as f:
+        writer = csv.writer(f)
+        reader = csv.reader(test)
+
+        writer.writerow(["url", "status"])
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+            var = "phishing" if L[i - 1] == 1 else "legitimate"
+            writer.writerow([row[1], var])
+    test.close()
+
+
+def build_submit_set(path):
+    df = pd.read_csv(path, header=0)
+    x = df._get_numeric_data()
+    x = x.drop(columns=['Unnamed: 0'])
+    x = x.to_numpy()
+    x = normalize(x, axis=0)
+    return x
 
 
 def filter(L):
@@ -67,19 +92,20 @@ grid = {
 # show start time
 print(datetime.now())  # Grid Search function
 
-CV_rfr = GridSearchCV(estimator=RandomForestClassifier(),
-                      param_grid=grid, cv=5, verbose=10, n_jobs=-1)
-CV_rfr.fit(x_train, y_train)
+# CV_rfr = GridSearchCV(estimator=RandomForestClassifier(),
+#                       param_grid=grid, cv=5, verbose=10, n_jobs=-1)
+# CV_rfr.fit(x_train, y_train)
 
 # show end time
 print(datetime.now())
-print("\n The best parameters across ALL searched params:\n", CV_rfr.best_params_)
+# print("\n The best parameters across ALL searched params:\n", CV_rfr.best_params_)
 
-
-rf = RandomForestClassifier(n_estimators=1200, max_features='sqrt', max_depth=28, random_state=18)
+rf = RandomForestClassifier(n_estimators=1000, max_features='sqrt', max_depth=28, random_state=18)
 
 rf.fit(x_train, y_train)
-print(rf.predict(x_test))
-prediction = filter(CV_rfr.predict(x_test))
+# print(rf.predict(x_test))
 
-print("Accuracy: ", compute_accuracy(y_test, prediction))
+x_test_submit = build_submit_set("test.csv")
+prediction = filter(rf.predict(x_test_submit))
+# print("Accuracy: ", compute_accuracy(y_test, prediction))
+submission(prediction)
