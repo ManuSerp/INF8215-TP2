7531fa087736825cc027322320595d80146f3f1e
Serp <manu.serpette74@gmail.com>
2023-04-13 15:05:07 -0400

    comit

--- fishing/train.py
+++ fishing/train.py
@@ -52,8 +52,11 @@ def compute_accuracy(Y_true, Y_pred):
 
 def build_set(path):
     df = pd.read_csv(path, header=0)
-    x = df._get_numeric_data()
-    x = x.drop(columns=['Unnamed: 0'])
+
+    selected_columns = ['length_url', 'length_hostname', "nb_dots", "nb_hyphens", "nb_at", "nb_qm", "nb_and", "nb_or", "nb_eq", "nb_underscore", "nb_tilde", "nb_percent", "nb_slash", "nb_star", "nb_colon", "nb_comma", "nb_semicolumn", "nb_dollar", "nb_space", "nb_www", "nb_com", "nb_dslash", "ratio_digits_url", "ratio_digits_host", "punycode", "path_extension", "nb_redirection", "nb_external_redirection",
+                        "ratio_intRedirection", "ratio_extRedirection", "login_form", "external_favicon", "sfh", "iframe", "popup_window", "safe_anchor", "onmouseover", "right_clic", "empty_title", "domain_in_title", "domain_in_brand", "brand_in_subdomain", "brand_in_path", "whois_registered_domain", "domain_registration_length", "domain_age", "web_traffic", "dns_record", "google_index", "page_rank"]
+    x = df[selected_columns]
+    x = x._get_numeric_data()
     x = x.to_numpy()
     x = normalize(x, axis=0)
     y = df["status"].to_numpy()
@@ -67,8 +70,10 @@ def build_set(path):
 
 def build_submit_set(path):
     df = pd.read_csv(path, header=0)
-    x = df._get_numeric_data()
-    x = x.drop(columns=['Unnamed: 0'])
+    selected_columns = ['length_url', 'length_hostname', "nb_dots", "nb_hyphens", "nb_at", "nb_qm", "nb_and", "nb_or", "nb_eq", "nb_underscore", "nb_tilde", "nb_percent", "nb_slash", "nb_star", "nb_colon", "nb_comma", "nb_semicolumn", "nb_dollar", "nb_space", "nb_www", "nb_com", "nb_dslash", "ratio_digits_url", "ratio_digits_host", "punycode", "path_extension", "nb_redirection", "nb_external_redirection",
+                        "ratio_intRedirection", "ratio_extRedirection", "login_form", "external_favicon", "sfh", "iframe", "popup_window", "safe_anchor", "onmouseover", "right_clic", "empty_title", "domain_in_title", "domain_in_brand", "brand_in_subdomain", "brand_in_path", "whois_registered_domain", "domain_registration_length", "domain_age", "web_traffic", "dns_record", "google_index", "page_rank"]
+    x = df[selected_columns]
+    x = x._get_numeric_data()
     x = x.to_numpy()
     x = normalize(x, axis=0)
     return x
@@ -80,23 +85,23 @@ x_train, x_test, y_train, y_test = train_test_split(
     x_train, y_train, test_size=0.3, shuffle=True)
 
 grid = {
-    'n_estimators': [900, 1000, 1200, 1500],
+    'n_estimators': [1200, 1500, 1800],
     'max_features': ['sqrt'],
-    'max_depth': [26, 28, 30],
+    'max_depth': [24, 28],
     'random_state': [18]
 }  # show start time
 print(datetime.now())  # Grid Search function
-CV_rfr = GridSearchCV(estimator=RandomForestRegressor(),
-                      param_grid=grid, cv=5, verbose=10, n_jobs=-1)
-CV_rfr.fit(x_train, y_train)  # show end time
-print(datetime.now())
-print("\n The best parameters across ALL searched params:\n", CV_rfr.best_params_)
+# CV_rfr = GridSearchCV(estimator=RandomForestRegressor(),
+#                       param_grid=grid, cv=5, verbose=10, n_jobs=-1)
+# CV_rfr.fit(x_train, y_train)  # show end time
+# print(datetime.now())
+# print("\n The best parameters across ALL searched params:\n", CV_rfr.best_params_)
 
 
-# rf = RandomForestRegressor(
-#     n_estimators=1000, max_features='sqrt', max_depth=24, random_state=18)
+rf = RandomForestRegressor(
+    n_estimators=1800, max_features='sqrt', max_depth=24, random_state=18)
 
-# rf.fit(x_train, y_train)
+rf.fit(x_train, y_train)
 
 #  {'max_depth': 26, 'max_features': 'sqrt', 'n_estimators': 800, 'random_state': 18} 0.9665
 
@@ -108,12 +113,12 @@ print("\n The best parameters across ALL searched params:\n", CV_rfr.best_params
 # Accuracy:  0.9706362153344209
 
 
-prediction = filter(CV_rfr.predict(x_test))
+# prediction = filter(CV_rfr.predict(x_test))
 
 x_test_submit = build_submit_set("test.csv")
 
-# prediction = filter(rf.predict(x_test))
+prediction = filter(rf.predict(x_test_submit))
 
-print("Accuracy: ", compute_accuracy(y_test, prediction))
+#print("Accuracy: ", compute_accuracy(y_test, prediction))
 
-# submission(prediction)
+submission(prediction)
